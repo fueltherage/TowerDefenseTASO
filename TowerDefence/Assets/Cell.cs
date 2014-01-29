1c75cdfae61e01f89ef25c6a8910de5f127497b3
fueltherage <wesleyallard1990@gmail.com>
2014-01-28 19:37:10 -0500

    Towerdefense, Basic grid generation

---
+++ TowerDefence/Assets/Cell.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Cell{
+	public int x;
+	public int y;
+	public bool canHaveTower;
+	
+}
---
+++ TowerDefence/Assets/Grid.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	// Use this for initialization
+	Cell[,] cells;
+	int gridWidth=10;
+	int gridHeight=10;
+	public GameObject cellObject;
+	void Start () {
+		GenerateGrid();	
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void GenerateGrid()
+	{
+		cells = new Cell[gridWidth, gridHeight];
+
+		for(int i=0; i<gridWidth; i++)
+		{
+			for(int j=0; j<gridHeight;j++)
+			{
+				cells[i,j].x = i;
+				cells[i,j].y = j;
+				Instantiate(cellObject, new Vector3(i,j,0),Quaternion.identity);
+				
+			}
+		}
+		
+
+	}
+}
